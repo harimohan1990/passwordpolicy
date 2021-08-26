820493d8cf323ef0f9c89b8f05781f7659c254ac
unknown <you@example.com>
2021-08-26 20:31:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+import { useState } from "react";
+import { usePasswordValidation } from "./components/usePasswordValidation";
+
+function App() {  
+
+const [password, setPassword] = useState({
+      firstPassword: "",
+      secondPassword: "",
+     });
+  
+const [
+    validLength,
+    hasNumber,
+    upperCase,
+    lowerCase,
+    match,
+    specialChar,
+] = usePasswordValidation({
+firstPassword: password.firstPassword,
+secondPassword: password.secondPassword,
+});
+
+
+const setFirst = (event) => {
+  setPassword({ ...password, firstPassword: event.target.value });
+};
+const setSecond = (event) => {
+  setPassword({ ...password, secondPassword: event.target.value });
+};
+return (
+<div className='App'>
+  <div>
+    First Password:
+    <input onChange={setFirst} type='text' />
+  </div>
+  <div>
+    Second Password:
+    <input onChange={setSecond} type='text' />
+  </div>
+  <div>
+    <ul>
+      <li>
+        Valid Length: {validLength ? <span>True</span> : <span>False</span>}
+      </li>
+      <li>
+        Has a Number: {hasNumber ? <span>True</span> : <span>False</span>}
+      </li>
+      <li>
+        UpperCase: {upperCase ? <span>True</span> : <span>False</span>}
+      </li>
+      <li>
+        LowerCase: {lowerCase ? <span>True</span> : <span>False</span>}
+      </li>
+      <li>Match: {match ? <span>True</span> : <span>False</span>}</li>
+      <li>
+        Special Character:{" "}
+        {specialChar ? <span>True</span> : <span>False</span>}
+      </li>
+  </ul>
+  </div>
+  </div>
   );
 }
-
 export default App;
